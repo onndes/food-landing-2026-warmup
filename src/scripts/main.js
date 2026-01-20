f6729f5210843ad08a2113aa96ba6fc725c98e11
onndes <vladvyljotnikov@gmail.com>
2026-01-20 21:58:50 +0200

    feat: add analytics section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,25 @@ const openBurger = () => {
   burger.classList.toggle('header__burger--opened');
 };
 burger.addEventListener('click', openBurger);
+
+// Select
+const select = document.querySelector('.analytics-select');
+const trigger = select.querySelector('.analytics-select__trigger');
+const options = select.querySelectorAll('.analytics-select__option');
+
+trigger.addEventListener('click', () => {
+  select.classList.toggle('analytics-select--open');
+});
+
+options.forEach((option) => {
+  option.addEventListener('click', () => {
+    trigger.textContent = option.textContent;
+    select.classList.remove('analytics-select--open');
+  });
+});
+
+document.addEventListener('click', (e) => {
+  if (!select.contains(e.target)) {
+    select.classList.remove('analytics-select--open');
+  }
+});
