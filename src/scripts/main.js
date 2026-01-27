4ccd8e7e08db842c1caf36d31c5c77e23a8dae1f
onndes <vladvyljotnikov@gmail.com>
2026-01-27 17:24:45 +0200

    feat: enhance comments section with dynamic progress line and improved styling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,3 +29,65 @@ document.addEventListener('click', (e) => {
     select.classList.remove('analytics-select--open');
   }
 });
+
+// Comments Progress Line
+const comments = document.querySelector('.comments');
+const track = comments.querySelector('.comments__track');
+const container = comments.querySelector('.comments__container');
+const progressLine = comments.querySelector('.comments__progress-line');
+
+const getCSSVarPx = (name, el = comments) =>
+  parseFloat(getComputedStyle(el).getPropertyValue(name));
+
+const getParametersData = () => {
+  const activeCard = comments.querySelector('.comments-card--active');
+  if (!activeCard) return null;
+
+  return {
+    containerWidth: getCSSVarPx('--container'),
+    cardPadding: getCSSVarPx('--card-padding'),
+    paddingHorizontalDesktop: getCSSVarPx('--padding-horizontal-desktop'),
+    cardWidth: activeCard.getBoundingClientRect().width,
+    trackPaddingLeft:
+      track.getBoundingClientRect().left -
+      container.getBoundingClientRect().left,
+    viewportWidth: document.documentElement.clientWidth,
+  };
+};
+
+const updateProgressLine = () => {
+  const data = getParametersData();
+  if (!data) return;
+
+  const {
+    containerWidth,
+    cardPadding,
+    paddingHorizontalDesktop,
+    cardWidth,
+    trackPaddingLeft,
+    viewportWidth,
+  } = data;
+
+  const fullWidth =
+    viewportWidth < containerWidth ? viewportWidth : containerWidth;
+
+  const left = Math.abs(trackPaddingLeft) + cardWidth + cardPadding * 3;
+  const width =
+    fullWidth - cardWidth - cardPadding * 3 - paddingHorizontalDesktop;
+
+  progressLine.style.left = `${left}px`;
+  progressLine.style.width = `${width}px`;
+};
+console.log(getCSSVarPx('--container'));
+const checkDesktop = () => document.documentElement.clientWidth > 1024;
+
+checkDesktop() && updateProgressLine();
+
+window.addEventListener('resize', () => {
+  if (checkDesktop()) {
+    updateProgressLine();
+  } else {
+    progressLine.style.removeProperty('left');
+    progressLine.style.removeProperty('width');
+  }
+});
